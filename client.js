c404a46874222db537d2bd502ed7dfe246608573
Lukas Nord <luke.nord@gmail.com>
2017-12-18 08:23:35 -0600

    refactored my repeated code into a seperate function for the monthly total update

--- client.js
+++ client.js
@@ -38,36 +38,9 @@ function formSubmit(){
   costArray.push( employee.monthlyCost() );
   //send employee data to the DOM
   var $row = $('tbody:last-child');
-
   $row.append('<tr><td>' + employee.firstName + '</td><td>' + employee.lastName + '</td><td>' + employee.idNum + '</td><td>' + employee.jobTitle + '</td><td>' + employee.annualSalary+ '</td><td class="monthlyCost">' + employee.monthlyCost() + '</td><td id="deleteButton"><button type="button" class="deleteRow">' + 'Delete' + '</button></td></tr>');
 
-  //  This whole Code block "worked", but wouldn't wrap my <td> block in a <tr> so i couldn't select for it later during the delete problem.
-  //  Had to resort to the above code to force wrap the <tr></tr> around the data.  Would like to revisit this and figure out what i'm missing.
-  // $row.append('<td>' + employee.firstName + '</td>');
-  // $row.append('<td>' + employee.lastName + '</td>');
-  // $row.append('<td>' + employee.idNum + '</td>');
-  // $row.append('<td>' + employee.jobTitle + '</td>');
-  // $row.append('<td>' + employee.annualSalary+ '</td>');
-  // $row.append('<td class="monthlyCost">' + employee.monthlyCost() + '</td>');
-  // $row.append('<td><button type="button" class="deleteRow">' + 'Delete' + '</button></td></tr>');
-  //$('#tableBody').append('<tr>'); //set the next row
-
-
-  //create local counters
-  var monthlyTotalCost = 0;
-  var totalDeletes = 0;
-  //Monthly total counter
-  for(var k= 0; k<costArray.length; k++){
-    monthlyTotalCost +=  costArray[k];
-  }
-  for(var i = 0; i<trackDeletes.length;i++){
-    totalDeletes += trackDeletes[i];
-
-  }//end for loop
-  var pushNewTotal = 0;
-  pushNewTotal = monthlyTotalCost - totalDeletes;
-  //push monthly total to the DOM
-  $('#showCost').text('$' +' ' + pushNewTotal);
+  updateMonthly(); //update monthly total
   //clear inputs
   $('input').val('');
 }//end formSubmit
@@ -78,19 +51,24 @@ function deleteRow(){
   if (confirm == true){
     //send deleted selection to an array.
     trackDeletes.push(Number($(this).parent().prev().text()));
-    //create local counters
-    var monthlyTotalCost = 0;
-    var totalDeletes = 0;
 
-    for(var i = 0; i<trackDeletes.length;i++){
-      totalDeletes += trackDeletes[i];
-    }//end for loop
-    for(var k= 0; k<costArray.length; k++){
-      monthlyTotalCost +=  costArray[k];
-    }//end for loop
-    var pushNewTotal = monthlyTotalCost - totalDeletes;
-    $('#showCost').text('$' + pushNewTotal);
-
+    updateMonthly();
     $(this).parent().parent().fadeOut();
   }//end if statement
 }//end deleteRow
+
+
+function updateMonthly(){
+  var monthlyTotalCost = 0;
+  var totalDeletes = 0;
+
+  for(var i = 0; i<trackDeletes.length;i++){
+    totalDeletes += trackDeletes[i];
+  }//end for loop
+  for(var k= 0; k<costArray.length; k++){
+    monthlyTotalCost +=  costArray[k];
+  }//end for loop
+  var pushNewTotal = monthlyTotalCost - totalDeletes;
+  $('#showCost').text('$' + pushNewTotal);
+
+}
